f831432601f9190554b83ebb8d61c2378dd110e6
digitalprojections <36181294+digitalprojections@users.noreply.github.com>
2018-08-13 23:53:32 +0900

    ads

--- www/js/main.js
+++ www/js/main.js
@@ -283,6 +283,9 @@ function addListeners()
         }, function (error) {
             console.error(error);
         });
+        initAd();
+        registerAdEvents();
+        
     }, false);
 
     ons.setDefaultDeviceBackButtonListener(function (event) {
@@ -373,3 +376,47 @@ function set_languages(event)
         }
     });
 }
+
+//initialize the goodies
+function initAd(){
+        if ( window.plugins && window.plugins.AdMob ) {
+            var ad_units = {
+                ios : {
+                    banner: 'ca-app-pub-3838820812386239/2551267023',		//PUT ADMOB ADCODE HERE
+                    interstitial: 'ca-app-pub-3838820812386239/2551267023'	//PUT ADMOB ADCODE HERE
+                },
+                android : {
+                    banner: 'ca-app-pub-3838820812386239/2551267023',		//PUT ADMOB ADCODE HERE
+                    interstitial: 'ca-app-pub-3838820812386239/2551267023'	//PUT ADMOB ADCODE HERE
+                }
+            };
+            var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
+ 
+            window.plugins.AdMob.setOptions( {
+                publisherId: admobid.banner,
+                interstitialAdId: admobid.interstitial,
+                adSize: window.plugins.AdMob.AD_SIZE.SMART_BANNER,	//use SMART_BANNER, BANNER, LARGE_BANNER, IAB_MRECT, IAB_BANNER, IAB_LEADERBOARD
+                bannerAtTop: false, // set to true, to put banner at top
+                overlap: true, // banner will overlap webview
+                offsetTopBar: false, // set to true to avoid ios7 status bar overlap
+                isTesting: false, // receiving test ad
+                autoShow: true // auto show interstitial ad when loaded
+            });
+ 
+            registerAdEvents();
+        } else {
+            //alert( 'admob plugin not ready' );
+        }
+}
+//functions to allow you to know when ads are shown, etc.
+function registerAdEvents() {
+        document.addEventListener('onReceiveAd', function(){});
+        document.addEventListener('onFailedToReceiveAd', function(data){});
+        document.addEventListener('onPresentAd', function(){});
+        document.addEventListener('onDismissAd', function(){ });
+        document.addEventListener('onLeaveToAd', function(){ });
+        document.addEventListener('onReceiveInterstitialAd', function(){ });
+        document.addEventListener('onPresentInterstitialAd', function(){ });
+        document.addEventListener('onDismissInterstitialAd', function(){ });
+    }
+ 
